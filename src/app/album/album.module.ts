80b885309b625b3d8bfe95aa344b93d3565b065a
Ruben Laube-Pohto <ruben@goodhum.com>
2019-06-24 18:28:36 +0300

    Clean up deps

--- src/app/album/album.module.ts
+++ src/app/album/album.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AlbumRoutingModule } from './album-routing.module';
-import { AlbumBrowserComponent } from './album-browser/album-browser.component';
-import { AlbumViewerComponent } from './album-viewer/album-viewer.component';
+import { NgModule } from '@angular/core';
 import { AppMaterialModule } from '../app-material.module';
 import { ComponentsModule } from '../components/components.module';
+import { AlbumBrowserComponent } from './album-browser/album-browser.component';
+import { AlbumRoutingModule } from './album-routing.module';
+import { AlbumViewerComponent } from './album-viewer/album-viewer.component';
 
 @NgModule({
     declarations: [AlbumBrowserComponent, AlbumViewerComponent],
--- src/app/photo-browser/photo-browser.module.ts
+++ src/app/photo-browser/photo-browser.module.ts
@@ -1,24 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { VirtualScrollerModule } from 'ngx-virtual-scroller';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { NgModule } from '@angular/core';
+import { ComponentsModule } from '../components/components.module';
 import { PhotoBrowserRoutingModule } from './photo-browser-routing.module';
 import { PhotoBrowserComponent } from './photo-browser.component';
-import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
     declarations: [PhotoBrowserComponent],
-    imports: [
-        CommonModule,
-        PhotoBrowserRoutingModule,
-        MatGridListModule,
-        ScrollingModule,
-        VirtualScrollerModule,
-        FlexLayoutModule,
-        ComponentsModule,
-    ],
+    imports: [CommonModule, PhotoBrowserRoutingModule, ComponentsModule],
 })
 export class PhotoBrowserModule {}
