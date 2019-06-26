3866de74511e1a58d7da3b0fb8fbbedea89d0835
Ruben Laube-Pohto <ruben@goodhum.com>
2019-06-26 19:58:04 +0300

    Clean up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,26 @@
-import { Component, ChangeDetectorRef, OnDestroy, ViewChild } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Router } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
 import { MatSidenavContainer } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
+    /** Query to check if using a small mobile screen */
     mobileQuery: MediaQueryList;
-    private mobileQueryListener: () => void;
     @ViewChild(MatSidenavContainer, { static: false }) sidenav: MatSidenavContainer;
 
-    constructor(private changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher, private router: Router) {
-        this.mobileQuery = media.matchMedia('(max-width: 600px)');
-        // this.mobileQueryListener = () => changeDetectorRef.detectChanges();
-        // this.mobileQuery.addEventListener('change', this.mobileQueryListener);
-    }
-
-    ngOnDestroy(): void {
-        // this.mobileQuery.removeEventListener('change', this.mobileQueryListener);
+    constructor(private router: Router, private media: MediaMatcher) {
+        this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
     }
 
     goToPage(route: any[]) {
         this.router.navigate(route);
-        if (this.mobileQuery.matches) this.sidenav.close();
+        if (this.mobileQuery.matches) {
+            this.sidenav.close();
+        }
     }
 }
